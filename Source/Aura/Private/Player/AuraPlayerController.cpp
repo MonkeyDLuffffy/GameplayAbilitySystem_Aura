3d79126bf6e735cd8b4b8005ba1553d0ec8ed927
xialuhui <615860963@qq.com>
2024-07-09 17:26:02 +0800

    create Aura and Goblin Bp and ABP

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,20 @@
+// copyright Druid bujingyun
+
+
+#include "Player/AuraPlayerController.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates=true;
+	
+
+	
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	
+}
